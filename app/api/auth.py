a5b72458a6b1642c0244c7580d69e8a04d4a7ceb
Askhat <askhat.ss23@gmail.com>
2025-03-18 10:25:37 +0500

    register stuff

--- app/api/auth.py
+++ app/api/auth.py
@@ -20,9 +20,11 @@ def login_access_token(
         db: Session = Depends(get_db),
         form_data: OAuth2PasswordRequestForm = Depends()
 ) -> Any:
+
     user = crud.user.authenticate(
-        db, email=form_data.username, password=form_data.password
+        db, form_data.username, form_data.password
     )
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -33,6 +35,7 @@ def login_access_token(
         raise HTTPException(status_code=400, detail="Inactive user")
 
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
     return {
         "access_token": create_access_token(
             user.id, expires_delta=access_token_expires
